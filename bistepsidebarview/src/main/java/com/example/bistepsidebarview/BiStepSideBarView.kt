0ba4fce836aae6be17dde0db4a909d2c2516279e
Anwesh43 <anweshthecool0@gmail.com>
2021-04-13 03:25:20 +0530

    adding BiStepSideBarView class

--- bistepsidebarview/src/main/java/com/example/bistepsidebarview/BiStepSideBarView.kt
+++ bistepsidebarview/src/main/java/com/example/bistepsidebarview/BiStepSideBarView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBSSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiStepSideBar(scale, w, h, paint)
 }
+
+class BiStepSideBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
