4547f050079a5942b8e22c1f7af99eb813673891
Anwesh43 <anweshthecool0@gmail.com>
2021-04-13 03:14:23 +0530

    adding constants and libraries

--- bistepsidebarview/src/main/java/com/example/bistepsidebarview/BiStepSideBarView.kt
+++ bistepsidebarview/src/main/java/com/example/bistepsidebarview/BiStepSideBarView.kt
@@ -1,2 +1,27 @@
 package com.example.bistepsidebarview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val bars : Int = 4
+val parts : Int = bars + 1
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.7f
+val lFactor : Float = 13.5f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#004D40",
+    "#0D47A1",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
